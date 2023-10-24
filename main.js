3f4fbb1265542c520ad289f961f3d07219c2ae3a
Ilyaszv <145338615+Ilyaszv@users.noreply.github.com>
2023-10-25 01:37:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function openbox(box) {
+  display=document.getElementById('box').style.display;
+  if (display=="none") {
+    document.getElementById('box').style.display="block";
+  } else{
+    document.getElementById('box').style.display="none";
+  }
+}
+function openbox1(box1) {
+  display=document.getElementById('box1').style.display;
+  if (display=="none") {
+    document.getElementById('box1').style.display="block";
+  } else{
+    document.getElementById('box1').style.display="none";
+  }
+}
